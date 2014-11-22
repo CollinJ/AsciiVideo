53da9db75669ddc2dee6fa0a034dd86ea06c3464
Collin Johnston <collin389@gmail.com>
2014-11-21 23:48:21 -0800

    PIXEL VIDEOS

--- app.py
+++ app.py
@@ -2,13 +2,12 @@
 import curses
 import numpy as np
 import cv2
+import cv2.cv as cv
 from time import sleep
-
 
 def main(stdscr):
   init(stdscr)
-  img = cv2.imread('test2.jpg')
-  drawImage(img, stdscr)
+  drawVideo("test.mp4", stdscr)
   stdscr.getch()
 
 def drawImage(img, stdscr):
@@ -17,23 +16,69 @@ def drawImage(img, stdscr):
   maxX -= 20
   height = len(img)
   width = len(img[0])
-  stdscr.clear()
   for y in xrange(height):
     for x in xrange(width):
-      if x >= maxX or y >= maxY:
+      if 2*x >= maxX or y >= maxY:
         continue
       rgb = img[y][x]
-      stdscr.addstr(y,x,str(" "), curses.color_pair(RGBto256Color(rgb)))
+      stdscr.addstr(y,2*x,str(" "), curses.color_pair(RGBto256Color(rgb)))
+      stdscr.addstr(y,2*x+1,str(" "), curses.color_pair(RGBto256Color(rgb)))
   stdscr.refresh()
+
+def drawVideo(file, stdscr):
+  cap = cv2.VideoCapture(file)
+  pixel_width = int(cap.get(cv.CV_CAP_PROP_FRAME_WIDTH))
+  pixel_height = int(cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
+  max_height, max_width = stdscr.getmaxyx()
+  max_height -= 3
+  max_width -= 3
+  
+  width = min(pixel_width, max_width)
+  height = int(width/2)
+  if (height > max_height):
+    height = max_height
+    width = height*2
+
+    
+
+  char_pixel_width = int(pixel_width/(1.0*width))
+  char_pixel_height = int(pixel_height/(1.0*height))
+
+  nframes=int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
+  fps= int(cap.get(cv.CV_CAP_PROP_FPS))
+  print "total frame",cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
+  print "fps" ,fps
+  print " currpos of videofile",cap.get(cv.CV_CAP_PROP_POS_MSEC)
+  waitpermillisecond=int(1*1000/fps)
+  print "waitpermillisecond",waitpermillisecond
+  print cap.get(cv.CV_CAP_PROP_FOURCC)
+
+  while(cap.isOpened()):
+    ret, frameimg=cap.read()
+    if not ret:
+      break
+    sleep(waitpermillisecond/1000.0)
+    print " currpos of videofile",cap.get(cv.CV_CAP_PROP_POS_MSEC)
+    print " index of frame",cap.get(cv.CV_CAP_PROP_POS_FRAMES)
+    drawImage(cv2.resize(frameimg,(width, height)), stdscr)
+    cv2.waitKey(1)
+
+  cap.release()
+  cv2.destroyAllWindows()
 
 
 def RGBto256Color(rgb):
-  r = int(rgb[0])
+  r = int(rgb[2])
   g = int(rgb[1])
-  b = int(rgb[2])
-  return ((r/32) << 5 |
-         ((g/32) >> 3) |
+  b = int(rgb[0])
+  i = ((r/32) << 5 |
+         ((g/32) << 2) |
          ((b/64)))
+  if i == 255:
+    return 0
+  if i == 0:
+    return 255
+  return i
 
 def init(stdscr):
   curses.noecho()
@@ -47,7 +92,8 @@ def init(stdscr):
     g = (i >> 2) & 7
     b = (i) & 3
     curses.init_color(i, r*142, g*142, int(b*333.334))
-  for i in range(0, curses.COLORS):
-    curses.init_pair(i + 1, -1, i)
+  curses.init_color(255, 0,0,0)
+  for i in range(0, 256):
+    curses.init_pair(i, -1, i)
 
 curses.wrapper(main)
--- image.py
+++ image.py
@@ -12,43 +12,44 @@ parser.add_option("-w", "--width", dest="width", help="number of characters wide
 (options, args) = parser.parse_args()
 
 
-cap = cv2.VideoCapture("test.mp4")
-pixel_width = int(cap.get(cv.CV_CAP_PROP_FRAME_WIDTH))
-pixel_height = int(cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
-width = pixel_width
-if options.width:
-  width = int(options.width)
+def drawVideo(file, stdscr):
+  cap = cv2.VideoCapture(file)
+  pixel_width = int(cap.get(cv.CV_CAP_PROP_FRAME_WIDTH))
+  pixel_height = int(cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
+  width = pixel_width
+  if options.width:
+    width = int(options.width)
 
-height = int(width/2.5)
-  
+  height = int(width/2.5)
+    
 
-char_pixel_width = int(pixel_width/(1.0*width))
-char_pixel_height = int(pixel_height/(1.0*height))
+  char_pixel_width = int(pixel_width/(1.0*width))
+  char_pixel_height = int(pixel_height/(1.0*height))
 
-print "Pixels: %sx%s" % (pixel_width, pixel_height)
-print "Chars: %sx%s" % (width, height)
-print "Char: %sx%s" % (char_pixel_width, char_pixel_height)
-exit()
+  print "Pixels: %sx%s" % (pixel_width, pixel_height)
+  print "Chars: %sx%s" % (width, height)
+  print "Char: %sx%s" % (char_pixel_width, char_pixel_height)
+  exit()
 
-nframes=int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
-fps= int(cap.get(cv.CV_CAP_PROP_FPS))
-print "total frame",cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
-print "fps" ,fps
-print " currpos of videofile",cap.get(cv.CV_CAP_PROP_POS_MSEC)
-waitpermillisecond=int(1*1000/fps)
-print "waitpermillisecond",waitpermillisecond
-print cap.get(cv.CV_CAP_PROP_FOURCC)
-
-while(cap.isOpened()):
-  ret, frameimg=cap.read()
-  if not ret:
-    break
-  sleep(waitpermillisecond/1000.0)
+  nframes=int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
+  fps= int(cap.get(cv.CV_CAP_PROP_FPS))
+  print "total frame",cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
+  print "fps" ,fps
   print " currpos of videofile",cap.get(cv.CV_CAP_PROP_POS_MSEC)
-  print " index of frame",cap.get(cv.CV_CAP_PROP_POS_FRAMES)
-  cv2.imshow("hcq",frameimg)
-  cv2.waitKey(1)
+  waitpermillisecond=int(1*1000/fps)
+  print "waitpermillisecond",waitpermillisecond
+  print cap.get(cv.CV_CAP_PROP_FOURCC)
 
-cap.release()
-cv2.destroyAllWindows()
+  while(cap.isOpened()):
+    ret, frameimg=cap.read()
+    if not ret:
+      break
+    sleep(waitpermillisecond/1000.0)
+    print " currpos of videofile",cap.get(cv.CV_CAP_PROP_POS_MSEC)
+    print " index of frame",cap.get(cv.CV_CAP_PROP_POS_FRAMES)
+    cv2.imshow("hcq",frameimg)
+    cv2.waitKey(1)
+
+  cap.release()
+  cv2.destroyAllWindows()
 
